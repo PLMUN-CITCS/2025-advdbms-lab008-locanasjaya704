b9319b050f2efef4d8a5ebf45d1ed30db3613f00
locanasjaya704 <locanasjayaeunice_bsit@plmun.edu.ph>
2025-02-15 16:44:16 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

--- bookstore_database/create_bookstore_schema.sql
+++ bookstore_database/create_bookstore_schema.sql
@@ -1,19 +1,20 @@
+-- Use the BookstoreDB database
 USE BookstoreDB;
 
-
+-- Books Table
 CREATE TABLE Books (
     ISBN VARCHAR(20) PRIMARY KEY,
     Title VARCHAR(255) NOT NULL,
     Price DECIMAL(5, 2) NOT NULL
 );
 
-
+-- Authors Table
 CREATE TABLE Authors (
     AuthorID INT PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(255) NOT NULL
 );
 
-
+-- BookAuthors Table (Linking Table)
 CREATE TABLE BookAuthors (
     ISBN VARCHAR(20),
     AuthorID INT,
@@ -22,14 +23,14 @@ CREATE TABLE BookAuthors (
     FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID)
 );
 
-
+-- Customers Table
 CREATE TABLE Customers (
     CustomerID INT PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(255) NOT NULL,
     Email VARCHAR(255)
 );
 
-
+-- Orders Table
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY AUTO_INCREMENT,
     CustomerID INT,
@@ -37,7 +38,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
-
+-- OrderDetails Table
 CREATE TABLE OrderDetails (
     OrderID INT,
     ISBN VARCHAR(20),
